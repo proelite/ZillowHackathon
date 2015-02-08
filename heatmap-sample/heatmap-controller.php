68205128718fd07eef402a46dfa1b476ab798635
Phil Napieralski <phil@pnapieralski.com>
2015-02-07 21:27:48 -0800

    Added heatmap sample

---
+++ heatmap-sample/heatmap-controller.php
@@ -0,0 +1,7 @@
+<?php
+
+// XXX TODO Use Kalapana's APIs to get actual crime data for a particular lat/lon
+
+$output["crimeData"] = Array( 0 => Array ("lat" => 37.782551, "lon" => -122.445368),
+							  1 => Array ("lat" => 37.78257, "lon" => -122.44544));
+?>
---
+++ heatmap-sample/index.php
@@ -0,0 +1,78 @@
+<?php
+// DON'T REMOVE THIS INCLUDE
+// Use the controller file for 'business' logic
+// This file (index.php) should only be view logic
+require('./heatmap-controller.php');
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Heatmaps</title>
+    <style>
+      html, body, #map-canvas {
+        height: 100%;
+        margin: 0px;
+        padding: 0px
+      }
+      #panel {
+        position: absolute;
+        top: 5px;
+        left: 50%;
+        margin-left: -180px;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+      }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=visualization"></script>
+    <script>
+
+var map, pointarray, heatmap;
+
+var crimeData = [
+<?php
+	if(isset($output["crimeData"]) && !empty($output["crimeData"]))
+	{
+		foreach( $output["crimeData"] as $crimeLatLon)
+		{
+			$lat = $crimeLatLon["lat"];
+			$lon = $crimeLatLon["lon"];
+			echo "new google.maps.LatLng($lat, $lon),";
+		}
+	}
+?>
+];
+
+function initialize() {
+  var mapOptions = {
+    zoom: 13,
+    center: new google.maps.LatLng(37.774546, -122.433523),
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+  var pointArray = new google.maps.MVCArray(crimeData);
+
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: pointArray
+  });
+
+  heatmap.setMap(map);
+  
+  heatmap.set('radius', 20);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+  </head>
+
+  <body>
+    <div id="map-canvas"></div>
+  </body>
+</html>
