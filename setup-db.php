1e7d4d9733b4f0fad53020ffa4ec7e23130726a7
vijay267 <vijay267@gmail.com>
2015-02-07 21:44:12 -0800

    Added Family Size Income Limits DB and PHP setup.

--- setup-db.php
+++ setup-db.php
@@ -6,4 +6,7 @@ require("Data/RentalHousing/import-housing-data.php");
 // Import all seattle school data available
 require("Data/School/import-schools-to-mysql-db.php");
 
+// Import all Family Size Income Limits
+require("Data/FamilySizeIncomeLimits/import-familysizeIncomeLimits-db.php");
+
 ?>
