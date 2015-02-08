7a77c4155205eaa2532a6c7e1c07f505a865645b
vijay267 <vijay267@gmail.com>
2015-02-07 22:56:50 -0800

    Link to map.html

--- list-rentals/index-controller.php
+++ list-rentals/index-controller.php
@@ -1,5 +1,7 @@
 <?php
 
+echo '<a href="../map.html"> Map </a> </br>';
+
 $input = $_GET;
 
 // Validate inputs, if nothing set use defaults
@@ -17,4 +19,35 @@ $input['householdIncome'] = intval($input['householdIncome']);
 
 $output = array( 0 => Array('Property Name' => 'Westside property', 'Typical Rent' => '1200 for your HH size'), 1 => Array('Property Name' => 'Westside property', 'Typical Rent' => '1200 for your HH size') );
 
+
+// Connect and setup db
+$db = mysqli_connect('localhost','root'); 
+
+if (!$db) 
+	die("Connection failed: " . mysqli_connect_error());
+
+$selectDB = mysqli_select_db($db, "HackHousing");	
+if (!$selectDB)
+	echo "Failed to select database </br>";
+   
+$query = "Select *";
+$query .= " FROM HUDHousingInfo, FamilySizeIncomeLimits";
+$query .= " (WHERE HUDHousingInfo.Housing_Authority == FamilySizeIncomeLimits.HousingAuthorityName)";
+   
+$buildingIDResult = mysqli_query($db, "Select * FROM HUDHousingInfo");   
+  
+if (!$buildingIDResult)
+{
+	 echo "Failed Query </br> ";
+	 die(mysqli_error($db));
+	 return;
+}
+
+if ($buildingIDResult->num_rows > 0)
+{
+	while ($row = $buildingIDResult->fetch_assoc())
+		echo "id: " . $row["NATIONAL_BUILDING_ID"] . ", Project Name: " . $row["PROJECT_NAME"] . "</br>";
+}
+
+mysqli_close($db);
 ?>
