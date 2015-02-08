6f407d0597bbac85d991ef5456e799bffdfcbb0c
proelite <xiaodi.huang@alumni.duke.edu>
2015-02-08 09:26:41 -0800

    Added heatmap code to map, map now shows crime heat map
    Added heatmap.

--- map-rentals/index-controller.php
+++ map-rentals/index-controller.php
@@ -60,5 +60,39 @@ if ($buildingIDResult->num_rows > 0)
 	}
 }
 
+require('../apis/getClosestCrimeData.php');
+
+$query = "SELECT BUILDING_NAME AS buildingName, STREET_ADDRESS As streetAddress, CITY AS city, STATE As state, LATITUDE AS lat, LONGITUDE as lon FROM `hudhousinginfo` WHERE city = 'Seattle'";
+
+$result = mysqli_query($db, $query) or die("Can't findr any section8 buildings!");
+
+function getLatLonCrimeWeight($lat, $lon)
+{
+	$crimeWeights = getCrimeDataWeights();
+	
+	$crimeData = getClosestCrimeData($lat, $lon);
+	
+	$weight = 1;
+	foreach( $crimeData as $crimeType => $count )
+	{
+		$crimeWeightAmt = 1;
+		if(!empty($crimeWeights[$crimeType]))
+		{
+			$crimeWeightAmt = $crimeWeights[$crimeType];
+		}
+		else
+		{
+			$crimeWeightAmt = $crimeWeights["OTHER"];
+		}
+		$weight *= $count * $crimeWeightAmt;
+	}
+	return $weight;
+}
+
+while( $row = mysqli_fetch_array($result))
+{
+	$crimes["crimeData"][] = Array ("lat" => $row["lat"], "lon" => $row["lon"], "weight" => getLatLonCrimeWeight($row["lat"], $row["lon"]));
+}
+
 require_once('../db-close.php');							  
 ?>
--- map-rentals/index.php
+++ map-rentals/index.php
@@ -9,32 +9,49 @@ require("index-controller.php");
 <!DOCTYPE html>
 <html>
   <head>
-    <style type="text/css">
-      html, body, #map-canvas { height: 100%; margin: 0; padding: 0;}
-    </style>
+	<style type="text/css">
+	  html, body, #map-canvas { height: 100%; margin: 0; padding: 0;}
+	</style>
 
-    <script type="text/javascript"
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC16FVIVK4DIVy_p6UwyBaekvYcgB_6OnM">
-    </script>
-    
-    <script type="text/javascript"
-      src="http://apicdn.walkscore.com/api/v1/traveltime_widget/js?wsid=f64c1f8a65290fca0c6cc0442dfe59b5">
-    </script>
-        
-    <script type="text/javascript">
+	<script type="text/javascript"
+	  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC16FVIVK4DIVy_p6UwyBaekvYcgB_6OnM&libraries=visualization">
+	</script>
+	
+	<script type="text/javascript"
+	  src="http://apicdn.walkscore.com/api/v1/traveltime_widget/js?wsid=f64c1f8a65290fca0c6cc0442dfe59b5">
+	</script>
+		
+	<script type="text/javascript">
 
-      const geocodeapi = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-      const apikey = '&key=AIzaSyC16FVIVK4DIVy_p6UwyBaekvYcgB_6OnM';
+	  const geocodeapi = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+	  const apikey = '&key=AIzaSyC16FVIVK4DIVy_p6UwyBaekvYcgB_6OnM';
 
-      var initialLocation;
-      var seattle = new google.maps.LatLng(47.61460,-122.31704);
-      var browserSupportFlag =  new Boolean();
-      var map;
-      var widget;
-      var loaded = false;
+	  var initialLocation;
+	  var seattle = new google.maps.LatLng(47.61460,-122.31704);
+	  var browserSupportFlag =  new Boolean();
+	  var map;
+	  var widget;
+	  var loaded = false;
+
+		var pointarray, heatmap;
+
+		var crimeData = [
+		<?php
+			if(isset($crimes["crimeData"]) && !empty($crimes["crimeData"]))
+			{
+				foreach( $crimes["crimeData"] as $crimeLatLon)
+				{
+					$lat = $crimeLatLon["lat"];
+					$lon = $crimeLatLon["lon"];
+					$weight = $crimeLatLon["weight"];
+					echo "new google.maps.LatLng($lat, $lon, $weight),";
+				}
+			}
+		?>
+		];
 
 	  var buildings = 
-	  	<?php
+		<?php
 			echo '\'{"Buildings":[';
 			if(isset($output["buildings"]) && !empty($output["buildings"]))
 			{   
@@ -98,121 +115,130 @@ require("index-controller.php");
 	  
 	  var buildingsJson;
 
-      function initialize() {
+	  function initialize() {
 
-        initialLocation = seattle;
+		initialLocation = seattle;
 
-        var mapOptions = {
-          mapTypeId: google.maps.MapTypeId.ROADMAP, center: initialLocation, zoom: 8, scrollwheel : false 
-        };
-        
-        map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
+		var mapOptions = {
+		  mapTypeId: google.maps.MapTypeId.ROADMAP, center: initialLocation, zoom: 4, scrollwheel : false 
+		};
+		
+		map = new google.maps.Map(document.getElementById('map-canvas'),
+			mapOptions);
 
-        // Try W3C Geolocation (Preferred)
-        if(navigator.geolocation) {
-          browserSupportFlag = true;
-          navigator.geolocation.getCurrentPosition(function(position) {
-            initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-            map.setCenter(initialLocation);
-          }, function() {
-            handleNoGeolocation(browserSupportFlag);
-          });
-        }
-        // Browser doesn't support Geolocation
-        else {
-          browserSupportFlag = false;
-          handleNoGeolocation(browserSupportFlag);
-        }
+		// Try W3C Geolocation (Preferred)
+		if(navigator.geolocation) {
+		  browserSupportFlag = true;
+		  navigator.geolocation.getCurrentPosition(function(position) {
+			initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+			map.setCenter(initialLocation);
+		  }, function() {
+			handleNoGeolocation(browserSupportFlag);
+		  });
+		}
+		// Browser doesn't support Geolocation
+		else {
+		  browserSupportFlag = false;
+		  handleNoGeolocation(browserSupportFlag);
+		}
 
-        widget = new walkscore.TravelTimeWidget({
-          map    : map,
-          origin : ''+initialLocation.lat()+','+initialLocation.lng(),
-          show   : true,
-          mode   : walkscore.TravelTime.Mode.DRIVE
-        });
+		widget = new walkscore.TravelTimeWidget({
+		  map    : map,
+		  origin : ''+initialLocation.lat()+','+initialLocation.lng(),
+		  show   : true,
+		  mode   : walkscore.TravelTime.Mode.DRIVE
+		});
 
-       	buildingsJson = JSON.parse(buildings);
+		buildingsJson = JSON.parse(buildings);
 
 		google.maps.event.addListenerOnce(map, 'idle', function(){
 			loaded = true;
-        	plotBuildings();
+			plotBuildings();
 		});
-     }
 
-    function handleNoGeolocation(errorFlag) {
-      initialLocation = seattle;
-      map.setCenter(initialLocation);
-    }
+		var pointArray = new google.maps.MVCArray(crimeData);
 
-    function submitLocation() 
-    {
-      if (loaded) 
-      {
-	      var address = document.getElementById("addressInput").value;
-	      var url = geocodeapi+address+apikey;
-	      var json = JSON.parse(httpGet(url));
-	      if (json.status=="OK") 
-	      {
-	        var latlng = json.results[0].geometry.location;
+		heatmap = new google.maps.visualization.HeatmapLayer({
+			data: pointArray
+		});
 
-	        map.setCenter(new google.maps.LatLng(latlng.lat, latlng.lng));
-	        widget.setOrigin(''+latlng.lat+','+latlng.lng);
-	        plotBuildings();
-	      }
-	      else
-	      {
-	        alert("Please enter a valid address");
-	      }	
-      }
-      else
-      {
-      	alert("Please wait for map to load.");
-      }
-    }
+		heatmap.setMap(map);  
+		heatmap.set('radius', 20);
+	 }
 
-    function plotBuildings()
-    {
-    	var bounds = map.getBounds();
-    	var i = 0;
+	function handleNoGeolocation(errorFlag) {
+	  initialLocation = seattle;
+	  map.setCenter(initialLocation);
+	}
 
-    	for (; i < buildingsJson.Buildings.length; i++)
-    	{
-    		var building = buildingsJson.Buildings[i];
-    		var latlng = new google.maps.LatLng(building.lat, building.lng);
-    		if (bounds.contains(latlng)) 
-    		{
+	function submitLocation() 
+	{
+	  if (loaded) 
+	  {
+		  var address = document.getElementById("addressInput").value;
+		  var url = geocodeapi+address+apikey;
+		  var json = JSON.parse(httpGet(url));
+		  if (json.status=="OK") 
+		  {
+			var latlng = json.results[0].geometry.location;
+
+			map.setCenter(new google.maps.LatLng(latlng.lat, latlng.lng));
+			widget.setOrigin(''+latlng.lat+','+latlng.lng);
+			plotBuildings();
+		  }
+		  else
+		  {
+			alert("Please enter a valid address");
+		  }	
+	  }
+	  else
+	  {
+		alert("Please wait for map to load.");
+	  }
+	}
+
+	function plotBuildings()
+	{
+		var bounds = map.getBounds();
+		var i = 0;
+
+		for (; i < buildingsJson.Buildings.length; i++)
+		{
+			var building = buildingsJson.Buildings[i];
+			var latlng = new google.maps.LatLng(building.lat, building.lng);
+			if (bounds.contains(latlng)) 
+			{
 				var marker = new google.maps.Marker({
-				    position: latlng,
-				    map: map,
-				    title: building.buildingName
+					position: latlng,
+					map: map,
+					title: building.buildingName
 				});
-    		}
-    	}
-    }
+			}
+		}
+	}
 
-    function httpGet(theUrl)
-    {
-        var xmlHttp = null;
+	function httpGet(theUrl)
+	{
+		var xmlHttp = null;
 
-        xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", theUrl, false );
-        xmlHttp.send( null );
-        return xmlHttp.responseText;
-    }
+		xmlHttp = new XMLHttpRequest();
+		xmlHttp.open( "GET", theUrl, false );
+		xmlHttp.send( null );
+		return xmlHttp.responseText;
+	}
 
-    </script>
+	</script>
 
   </head>
   <body onload="initialize()">
-    <div class="container">
-          <label class="input-label">
-            Where do you work?
-          <input id = "addressInput" type="text" name="location" placeholder="Enter a city, address, school, business, etc.">
-          </label>
-          <button type="button" onclick="submitLocation()">See Homes</button> 
-    </div>
-    <div id="map-canvas"></div>
+	<div class="container">
+		  <label class="input-label">
+			Where do you work?
+		  <input id = "addressInput" type="text" name="location" placeholder="Enter a city, address, school, business, etc.">
+		  </label>
+		  <button type="button" onclick="submitLocation()">See Homes</button> 
+	</div>
+	<div id="map-canvas"></div>
   </body>
 </html>
 
